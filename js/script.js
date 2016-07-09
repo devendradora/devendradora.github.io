b18924266a187541d3d5f69d6b408ca8515d3c26
Devendra Dora <dev.tech24@gmail.com>
2016-07-09 18:05:49 +0530

    Fixed home page responsiveness

--- js/script.js
+++ js/script.js
@@ -34,13 +34,7 @@ $('body').scrollspy('refresh'); // Refresh the scrollspy.
 
     new WOW().init();
 
-    // $('.skill').circliful({
-    //   fgcolor: "#E64A3B",
-    //   fontsize: "20",
-    //   bgcolor: "#D8CDBB",
-    //   dimension: "150"
-
-    // });
+   
 
 
     $('.has-shadow').append('<div class="shadow"></div>');
